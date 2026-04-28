f53232eda9883957ae861e60d3d667c3e1f4484c
Thomas <19thomastrimble@gmail.com>
2026-04-27 23:47:53 -0700

    BART API transition, Checkpoint 1 (My sleep deprived stupor that actually somehow worked)

--- finalScript.js
+++ finalScript.js
@@ -1,3 +1,51 @@
-let x = 0;
+let bartKey = "MW9S-E7SL-26DU-VV8V";
 
-let dKey = "AIzaSyCWuK9FvW8jJPwHc4hHwsJvCezf3X3KHLo";
+let station = "24th";
+
+let locationURL = `https://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=${station}&key=${bartKey}&json=y`;
+let arrivalURL = `https://api.bart.gov/api/etd.aspx?cmd=etd&orig=${station}&key=${bartKey}&json=y`
+
+let nameElement = document.getElementById("name");
+let locationElement = document.getElementById("location");
+let arrivalElement = document.getElementById("arrival");
+let arrivalToElement = document.getElementById("arrivalTo");
+
+let address = "";
+let sName = "";
+let nextArrival = "";
+let nextArrivalTime = ""
+
+getLocationInfo();
+getArrivalInfo();
+
+async function getLocationInfo() {
+    let response = await fetch(locationURL);
+
+    let parsed = await response.json();
+
+    sName = parsed.root.stations.station.name;
+
+    address = String(parsed.root.stations.station.city + ", " + parsed.root.stations.station.address);
+
+    nameElement.textContent += sName;
+    locationElement.textContent += address;
+}
+
+async function getArrivalInfo() {
+    let response = await fetch(arrivalURL);
+
+    let parsed = await response.json();
+
+    if (parsed.root.station[0].etd[0].estimate[0].minutes > parsed.root.station[0].etd[1].estimate[0].minutes) {
+        nextArrival = parsed.root.station[0].etd[1].destination
+        nextArrivalTime = parsed.root.station[0].etd[1].estimate[0].minutes
+    } else {
+        nextArrival = parsed.root.station[0].etd[0].destination
+        nextArrivalTime = parsed.root.station[0].etd[0].estimate[0].minutes
+    }
+
+    arrivalElement.textContent += nextArrivalTime + " minutes";
+    arrivalToElement.textContent += nextArrival;
+}
+
+
